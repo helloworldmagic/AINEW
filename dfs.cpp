8cd4821ed6db2a471b1fed514487a1cd791daa4f
helloworldmagic <104927327+helloworldmagic@users.noreply.github.com>
2022-05-11 11:01:57 +0530

    Update dfs.cpp

--- dfs.cpp
+++ dfs.cpp
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define v 4
 
-void DFS(int start , int v, int **adj, vector<bool>& visited){
+void DFS(int start ,int adj[v][v], vector<bool>& visited){
    cout << start << " ";
     visited[start] = true;
     for (int i = 0; i < v; i++) {
         if (adj[start][i] == 1 && (!visited[i])) {
-            DFS(i, v, adj, visited);
+            DFS(i, adj, visited);
         }
     }
 
@@ -14,22 +15,13 @@ void DFS(int start , int v, int **adj, vector<bool>& visited){
   
 
 int main(){
-   int v,e;
-   cout<<"Enter no of vertex : ";
-   cin>>v;
+   int e;
+
    cout<<"Enter no of edeges : ";
    cin>>e;
     
-    int *adj = new int[v];
-
-    for (int i=0;i<v;i++) {
-        adj[i] = new int[v];
-
-        for (int j=0;j<v;j++) {
-            adj[i][j] = 0;
-        }
-    }
- 
+    int adj[v][v];
+  
     for(int i=0;i<e;i++)
     {  
        int x,y;
@@ -37,13 +29,14 @@ int main(){
        cin>>x>>y;
        
        //undirected graph
-       adj[x][y] = 1;
-       adj[y][x] = 1;
+       // adj[x][y] = 1;
+       // adj[y][x] = 1;
 
        //directedgraph
-       //adj[x][y] = 1;
+       adj[x][y] = 1;
 
     }
+  
     vector<bool> visited(v, false);
-    DFS(0, v, adj, visited);
+    DFS(0, adj, visited);
 }
